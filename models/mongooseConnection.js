2802e717c375b50c346dd9ba010be7ec1d506c4c
Ramon Moraes <ramon.moraes94@gmail.com>
2017-11-10 13:30:46 -0300

    changeDb

--- models/mongooseConnection.js
+++ models/mongooseConnection.js
@@ -3,7 +3,8 @@ var db = mongoose.connection;
 
 mongoose.Promise = global.Promise; // Parar warning de Promises
 
-mongoose.connect('mongodb://devadmin:devadmin@ds149905.mlab.com:49905/lolrage',{useMongoClient: true});
+// mongoose.connect('mongodb://devadmin:devadmin@ds149905.mlab.com:49905/lolrage',{useMongoClient: true});
+mongoose.connect('mongodb://devadmin:devadmin@ds155315.mlab.com:55315/heroku_dg7dclm9',{useMongoClient: true});
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
